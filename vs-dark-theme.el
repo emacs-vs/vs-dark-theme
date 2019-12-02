b54b507f69a1f375c61a55cf94c572db2302cfa6
jenchieh <jcs090218@gmail.com>
2019-12-02 19:51:47 +0800

    Add vs dark theme.

---
+++ vs-dark-theme.el
@@ -0,0 +1,87 @@
+;;; vs-dark-theme.el --- Visual Studio IDE dark theme
+
+;; Copyright (C) 2019 , Jen-Chieh Shen
+
+;; Author: Jen-Chieh Shen
+;; URL: https://github.com/jcs090218/vs-dark-theme
+;; Version: 0.1
+;; Package-Requires: ((emacs "24.1"))
+;; Created with emacs-theme-generator, https://github.com/mswift42/theme-creator.
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Visual Studio IDE dark theme.
+;;
+
+;;; Code:
+
+(deftheme vs-dark
+  "Visual Studio IDE dark theme.")
+
+(let ((class '((class color) (min-colors 89)))
+      (fg1 "#D2D2D2")
+      (fg2 "#bfbfbf")
+      (fg3 "#adadad")
+      (fg4 "#9b9b9b")
+      (bg1 "#161616")
+      (bg2 "#252525")
+      (bg3 "#353535")
+      (bg4 "#464646")
+      (key2 "#50aafc")
+      (key3 "#208ddc")
+      (builtin "#B0C4DE")
+      (keyword "#17A0FB")
+      (const   "#38EFCA")
+      (comment "#6B8E23")
+      (func    "#D2D2D2")
+      (str     "#D69D78")
+      (type    "#38EFCA")
+      (var     "#D2D2D2"))
+  (custom-theme-set-faces
+   'vs-dark
+   `(default ((,class (:background ,bg1 :foreground ,fg1))))
+   `(font-lock-builtin-face ((,class (:foreground ,builtin))))
+   `(font-lock-comment-face ((,class (:foreground ,comment))))
+   `(font-lock-negation-char-face ((,class (:foreground ,const))))
+   `(font-lock-reference-face ((,class (:foreground ,const))))
+   `(font-lock-constant-face ((,class (:foreground ,const))))
+   `(font-lock-doc-face ((,class (:foreground ,comment))))
+   `(font-lock-function-name-face ((,class (:foreground ,func :bold t))))
+   `(font-lock-keyword-face ((,class (:bold ,class :foreground ,keyword))))
+   `(font-lock-string-face ((,class (:foreground ,str))))
+   `(font-lock-type-face ((,class (:foreground ,type ))))
+   `(font-lock-variable-name-face ((,class (:foreground ,var))))))
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;###autoload
+(defun vs-dark-theme ()
+  "Load Visual Studio dark theme."
+  (load-theme 'vs-dark t))
+
+(provide-theme 'vs-dark)
+(provide 'vs-dark-theme)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; vs-dark-theme.el ends here
